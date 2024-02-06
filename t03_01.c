f60aa74a79e9010b12f78eda96f27f467dd443d7
AriellaSihombing <ellasihombing12@gmail.com>
2024-02-06 21:25:42 +0700

    12S23005_Ariella U.C Sihombing

--- t03_01.c
+++ t03_01.c
@@ -1,11 +1,24 @@
-// NIM - NAMA
+// 12S23005 - Ariella U.C Sihombing
 
 #include <stdio.h>
 
 int main(int _argc, char **_argv)
 {
-
-  // your code here
-
+int n, max, min ;
+scanf("%d", &n) ;
+int angka [n] ;
+for (int i=0; i<n; i++)
+scanf("%d", &angka[i]) ;  
+max = angka[0] ;
+min = angka[0] ;
+for (int i=0; i<n; i++)
+{
+if (angka[i] > max)
+max = angka[i] ;
+if (angka[i] < min)
+min = angka[i] ;
+}
+printf("%d\n", min) ;
+printf("%d\n", max) ;
   return 0;
 }
